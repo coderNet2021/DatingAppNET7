00d46642e23babfb56f08d510cd85394c2178ca0
coderNet2021 <cnss.saida@gmail.com>
2023-09-22 01:21:39 +0200

    using async pipe with our own observable currentUser$ from the navComponent then from the AccountService easier

--- client16/src/app/nav/nav.component.ts
+++ client16/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/User';
 
 @Component({
   selector: 'app-nav',
@@ -9,26 +11,35 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit{
 
   title ="Dating App 16";
-  model : any={}
-  loggedIn=false;
-  constructor(private accountService:AccountService){
+  model : any={};
+  //loggedIn=false;
+  // currentUser$: Observable<User | null>= of(null);
+  constructor(public accountService:AccountService){
 
   }
   ngOnInit(): void {
+    // this.currentUser$ = this.accountService.currentUser$
+    // this.getCurrentUser();
   }
+
+  // getCurrentUser(){
+  //   this.accountService.currentUser$.subscribe({
+  //     next:user=>this.loggedIn=!!user,
+  //     error:err=>console.log(err)
+  //   })
+  // }
 
   login(){
 console.log(this.model);
 this.accountService.login(this.model).subscribe({
       next:(response)=>{
         console.log(response);
-        this.loggedIn=true;
       },
       error:(err)=>{console.log(err)},
       //complete:
     });
   }
   logout(){
-    this.loggedIn=false;
+    this.accountService.logout();
   }
 }
