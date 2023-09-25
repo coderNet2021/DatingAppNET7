5b90ad9ed7c7d92b57797c000961f04223c81328
coderNet2021 <cnss.saida@gmail.com>
2023-09-25 19:25:56 +0200

    finishing with registration

--- client16/src/app/register/register.component.ts
+++ client16/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,18 +7,25 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() usersFromHomeComponent :any;
   @Output() cancelRegister = new EventEmitter();
   model:any={};
 
-  constructor(){}
-  ngOnInit(): void {
-
+  constructor(private accountService : AccountService){
 
   }
 
+  ngOnInit(): void {
+  }
+
   register(){
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next:()=>{
+        //console.log(response);
+        // if we want to keep it we should do return in the map of the account service , register method.
+        this.cancel();
+      },
+      error: err=>console.log(err)
+    });
   }
 
   cancel(){
