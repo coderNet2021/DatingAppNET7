b41d1f0eea72b9313203c699af1cadeed89493e8
coderNet2021 <cnss.saida@gmail.com>
2024-02-26 02:19:17 +0100

    section 12- transforming the form from template form to reactive form, so here compare the code of template form and reactive form

--- client16/src/app/register/register.component.ts
+++ client16/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,27 +11,37 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model:any={};
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService : AccountService,
     private toastr:ToastrService){
 
   }
-
+initializeForm(){
+  this.registerForm= new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+    confirmPassword: new FormControl()
+  })
+}
   ngOnInit(): void {
+    this.initializeForm();
+
   }
 
   register(){
-    this.accountService.register(this.model).subscribe({
-      next:()=>{
-        //console.log(response);
-        // if we want to keep it we should do return in the map of the account service , register method.
-        this.cancel();
-      },
-      error: err=>{
-        console.log(err);
-        this.toastr.error(err.error.errors.Password);
-      }
-    });
+    console.log(this.registerForm?.value)
+    // this.accountService.register(this.model).subscribe({
+    //   next:()=>{
+    //     //console.log(response);
+    //     // if we want to keep it we should do return in the map of the account service , register method.
+    //     this.cancel();
+    //   },
+    //   error: err=>{
+    //     console.log(err);
+    //     this.toastr.error(err.error.errors.Password);
+    //   }
+    // });
   }
 
   cancel(){
