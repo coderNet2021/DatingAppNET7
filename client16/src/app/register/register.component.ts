7ecc0a3c58f921be973f570ee5589ab2ea816e97
coderNet2021 <cnss.saida@gmail.com>
2024-02-27 23:22:26 +0100

    section 12 - part adding validators to inputs, angular validators and custom validator for comparing matching inputs

--- client16/src/app/register/register.component.ts
+++ client16/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -19,11 +19,21 @@ export class RegisterComponent implements OnInit {
   }
 initializeForm(){
   this.registerForm= new FormGroup({
-    username: new FormControl(),
-    password: new FormControl(),
-    confirmPassword: new FormControl()
-  })
+    username: new FormControl('hello',Validators.required) ,
+    password: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+    confirmPassword: new FormControl('',[Validators.required,this.matchValue('password')])
+  });
+  this.registerForm.controls['password'].valueChanges.subscribe({
+    next:()=>this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+  });
 }
+
+matchValue(matchTo:string):ValidatorFn{
+return (control:AbstractControl)=>{
+  return control.value===control.parent?.get(matchTo)?.value ? null :{isMatching:true};
+}
+}
+
   ngOnInit(): void {
     this.initializeForm();
 
