a5f501dbdf463faf4c3c5a12a05e5063c0cd1a52
coderNet2021 <cnss.saida@gmail.com>
2024-03-31 16:06:09 +0200

    section 13 : adding pagination client side using valor angular bootstrap, and adjusting a little bit the code

--- client16/src/app/members/member-list/member-list.component.ts
+++ client16/src/app/members/member-list/member-list.component.ts
@@ -34,4 +34,13 @@ export class MemberListComponent implements OnInit {
    })
   }
 
+  pageChanged(event : any){
+    if(this.pageNumber !==event.page){
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
+
+
+  }
+
 }
