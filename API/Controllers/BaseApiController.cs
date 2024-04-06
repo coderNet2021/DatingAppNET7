f46166638c714671cd113c1554a804cbeac1a486
coderNet2021 <cnss.saida@gmail.com>
2024-04-06 18:13:04 +0200

    section 13 : adding the action filter to update the LastActive property

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")] // so when we want to hit this controller we write localhost..../api/users
     public class BaseApiController : ControllerBase
