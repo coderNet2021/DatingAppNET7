222799987f4f91ced6b8db6c668736ab0e463854
coderNet2021 <cnss.saida@gmail.com>
2024-04-14 15:14:05 +0200

    section 14 : finishing the like listing : users i like, and users who like me

--- client16/src/app/_services/members.service.ts
+++ client16/src/app/_services/members.service.ts
@@ -124,9 +124,9 @@ export class MembersService {
     return this.http.post(this.baseUrl + 'likes/' + username, {})
   }
 
-  getLikes(predicate: string, pageNumber: number) {
+  getLikes(predicate: string) {
 
-    return this.http.get(this.baseUrl+'likes?predicate='+predicate);
+    return this.http.get<Member[]>(this.baseUrl+'likes?predicate='+predicate);
   }
 
 
