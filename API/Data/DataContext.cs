86a8d80c90c6dafb3c8098e1ead1cd6c63cb76d9
coderNet2021 <cnss.saida@gmail.com>
2024-04-16 20:19:06 +0200

    section 15 : preparing for entity framework migrations add

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -34,6 +34,16 @@ namespace API.Data
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.TargetUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Message>()
+                           .HasOne(u => u.Recipient)
+                           .WithMany(m => m.MessagesReceived)
+                           .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
