e200ec11782ea7548ba49518fa0f5a44fec58588
coderNet2021 <cnss.saida@gmail.com>
2024-06-10 02:46:12 +0200

    adding the html for message threads

--- client16/src/app/members/member-messages/member-messages.component.ts
+++ client16/src/app/members/member-messages/member-messages.component.ts
@@ -1,30 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
 import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-messages',
-  standalone:true,
+  standalone: true,
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, TimeagoModule, FormsModule],
 })
-export class MemberMessagesComponent implements OnInit{
-  @Input() username?:string;
-  messages : Message[]=[];
-  constructor(private messageService:MessageService){}
+export class MemberMessagesComponent implements OnInit {
+  @Input() username?: string;
+  messages: Message[] = [];
+  constructor(private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.loadMessages();
   }
 
-  loadMessages(){
-    if(this.username){
+  loadMessages() {
+    if (this.username) {
       this.messageService.getMessageThread(this.username).subscribe({
-        next:messages => this.messages = messages
-      })
+        next: (messages) => (this.messages = messages),
+      });
     }
   }
-
 }
